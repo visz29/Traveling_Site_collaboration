5d34187ff2016ac3fbade1e7ff0c391f785831bd
Vishal Bhardwaj <156419780+visz29@users.noreply.github.com>
2025-07-11 07:28:56 +0000

    Structure fix

--- TravelNepal/src/components/NavigationBar.jsx
+++ TravelNepal/src/components/NavigationBar.jsx
@@ -4,7 +4,7 @@ function NavigationBar(){
 
 
     return(
-        <div>
+        <div className="fixed top-0 left-0 h-1/10 w-full bg-white/50">
             <h1> Nvigation Bar</h1>
         </div>
     )
--- TravelNepal/src/pages/Home.jsx
+++ TravelNepal/src/pages/Home.jsx
@@ -8,8 +8,8 @@ function Home(){
             <NavigationBar/>
             <main>
                 <div className="page1 w-screen h-screen bg-red-400">Page1</div>
-                <div className="page2 w-screen h-screen bg-bule-400"></div>
-                <div className="page3 w-screen h-screen bg-green-400"></div>
+                <div className="page2 w-screen h-screen bg-blue-400">Page2</div>
+                <div className="page3 w-screen h-screen bg-green-400">Page3</div>
             </main>
        </div>
     )
