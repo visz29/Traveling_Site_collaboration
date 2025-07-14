9e703115c7d173f3614783842dbc39f3a5f41cca
Vishal Bhardwaj <156419780+visz29@users.noreply.github.com>
2025-07-14 19:52:28 +0530

    new

--- TravelNepal/src/pages/Home.jsx
+++ TravelNepal/src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home(){
        <div className=" w-screen h-screen">
             <NavigationBar/>
             <main>
-                <div className="page1 w-screen h-screen bg-red-400">Page1</div>
+                <div className="page1 w-screen h-screen bg-red-400">Page1 with vishal</div>
                 <div className="page2 w-screen h-screen bg-blue-400">Page2</div>
                 <div className="page3 w-screen h-screen bg-green-400">Page3</div>
             </main>
